6d29b83f4fbbcd40a74146a2d8985da8bda9b93d
yaoyan <yaoyan@souche.com>
2020-07-22 18:08:57 +0800

    其他方法ok

--- other.js
+++ other.js
@@ -2,18 +2,20 @@
     promise all 全部 可以实现等待所以的异步执行完后 拿到统一结果
     解决异步并发 同步处理结果
 */
-const Promise = require('./promise/index2');
+// const Promise = require('./promise/index2');
 
 const fs = require('fs');
 
 function read(url) {
     // 可以解决嵌套问题 但是官方的promise并没有这个方法
-    let dfd  = Promise.defer();
+    /* let dfd  = Promise.defer();
     fs.readFile(url,'utf8',function(err,data){
         if(err) dfd.reject(err);
         dfd.resolve(data);
     })
-    return dfd.promise;
+    return dfd.promise; */
+
+    return new Promise()
 }
 
 /* 
@@ -89,3 +91,24 @@ Promise.all = function(values){
 /* 
     finally 最终的
 */
+Promise.prototype.finally = function(cb){
+    return this.then(data=>{
+
+        /* 
+            有可能finally 里面return了一个promise 所以需要用
+            resolve  promise.resolve可以等待那个promise执行完成后再执行
+        */
+        return Promise.resolve(cb()).then(()=>data);
+    },err => {
+        return Promise.resolve(cb()).then(()=>{throw err});
+    })
+}
+
+
+
+read('./name.txt').finally(data=>{
+    console.log(data,'finally')
+}).then(data=>{
+    console.log(data,'then')
+})
+console.log(read());
