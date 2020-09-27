5dbe278f74638ab78a87d4158322c9ef0331cb25
yaoyan <yaoyan@souche.com>
2020-09-27 18:48:17 +0800

    提交

--- then(test).js
+++ then(test).js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-22 10:24:54
+ * @LastEditTime: 2020-09-27 18:44:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /promise-my/then(test).js
+ */
 /* 
 then的原理
 1、then中传递的函数 判断成功和失败函数的返回结果
@@ -40,3 +48,15 @@ let p = new Promise((resolve,reject)=>{
 p.then().then().then(data=>{
     console.log(data);
 })
+
+Promise.myall = function(list){
+    let list = [],
+        index = 0;
+    return new Promise((r,j)=>{
+        for(let x in list){
+            if(typeof x.then === 'function'){
+
+            }
+        }
+    })
+}
